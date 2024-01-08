42bbc6e7e27cba2f03f53d29a8f74bbbe6f8cc4c
summer <summer@clobot.co.kr>
2024-01-08 09:53:45 +0900

    [ADD] useState

--- src/MemberItems.js
+++ src/MemberItems.js
@@ -4,13 +4,12 @@ import ItemDesc from './ItemDesc';
 import {useState} from 'react';
 
 const MemberItems = (props) => {
-    let name = props.name;
     const MemberBtnFn = () => {
         console.log('click!');
-        name = '새이름';
+        setName('겸겸');
     };
-    const test = useState();
-    console.log(test, '<<<summer');
+    const [name, setName] = useState(props.name);
+    console.log(name, setName, ':::name');
     return (
         <>
             <ul className="list_body">
@@ -18,7 +17,7 @@ const MemberItems = (props) => {
                     <div className="list_img"><img src={props.thumb} alt={props.thumb}/></div>
                     <ItemDesc name={name} email={props.email}/>
                     <button onClick={MemberBtnFn}>click!
-                        {/*소괄호 사용하면 안됨,  jsx코드가 반환될때 코드의 라인들이 웹브라우저의 해석기에 의해 평가됨*/}
+                        {/*소괄호 사용하면 안됨,  jsx 코드가 반환될때 코드의 라인들이 웹브라우저의 해석기에 의해 평가됨*/}
                     </button>
                 </li>
             </ul>
