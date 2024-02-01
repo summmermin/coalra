d0e9a8343e3040092ac59e24ca8c960cc8f6da35
summer <110873921+summmermin@users.noreply.github.com>
2024-02-01 18:26:01 +0900

    [DEL] 폴더 정리

--- components/ui/Heading.js
+++
@@ -1,7 +0,0 @@
-import './style/Heading.css';
-
-const Heading = (props) => {
-    return <div className="heading">{props.content}</div>;
-
-};
-export default Heading;
--- components/ui/Row.js
+++
@@ -1,7 +0,0 @@
-import './style/Row.css';
-
-const Row = (props) => {
-    return <div className="row">{props.children}</div>;
-
-};
-export default Row;
