3be99431a0df39b35881ebec553ca4110d2ded88
summer <110873921+summmermin@users.noreply.github.com>
2024-02-01 10:21:02 +0900

    [MOD] nav 부분 수정

---
+++ main/PageView.js
@@ -0,0 +1,11 @@
+import Header from '../header/Header';
+import Table from '../table/Table';
+
+const PageView = () => {
+    return <>
+        <div className="admin-main-contaienr w-[100%]">
+            <Table/>
+        </div>
+    </>;
+};
+export default PageView;
---
+++ nav/Navigation.js
@@ -0,0 +1,13 @@
+const Navigation = () => {
+    return <>
+        <div className="navigation">
+            <ul className="navigation__list">
+                <li className="navigation__item"><a href="">테스트1</a></li>
+                <li className="navigation__item"><a href="">테스트2</a></li>
+                <li className="navigation__item"><a href="">테스트3</a></li>
+                <li className="navigation__item"><a href="">테스트4</a></li>
+            </ul>
+        </div>
+    </>;
+};
+export default Navigation;
